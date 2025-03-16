e7737de6a9c5d678873cd9cb4c5ee8a1c93e018a
vanduongg <103979655+vanduongg@users.noreply.github.com>
2025-03-16 12:11:52 +0700

    [ADD][FIX] build UI

--- utils/constants.js
+++ utils/constants.js
@@ -1,7 +1,7 @@
 export const CONFIG = {
   // baseUrl: 'https://uat-erp.lrt.com.vn/api',
   // baseUrl: 'https://capstone-project-iot-1.onrender.com/api',
-  baseUrl: 'http://192.168.1.75:8000/api',
+  baseUrl: 'http://192.168.1.52:8000/api',
 };
 
 export const IMAGES = {
