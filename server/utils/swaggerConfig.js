fac9fe884b9c558dfee0f4c96e28018fb67611b0
Hau2223 <nguyentrunghau220203@gmail.com>
2025-03-28 16:29:52 +0700

    [FIX] new fix

--- server/utils/swaggerConfig.js
+++ server/utils/swaggerConfig.js
@@ -31,7 +31,7 @@ const options = {
     ],
     servers: [
       {
-        url: 'http://192.168.1.158:8000',
+        url: 'https://capstone-project-iot-1.onrender.com',
         // //https://capstone-project-iot-1.onrender.com
         // //http://localhost:8000
         // //http://192.168.0.241:8000
