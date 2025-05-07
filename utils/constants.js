816ae2540f21469ed82bb5a20c5713e76798d1f4
Hau2223 <nguyentrunghau220203@gmail.com>
2025-05-07 16:31:42 +0700

    [FIX] fix url

--- utils/constants.js
+++ utils/constants.js
@@ -1,13 +1,13 @@
 export const CONFIG = {
-  // baseUrl: 'https://capstone-project-iot-1.onrender.com/api',
-  baseUrl: 'http://192.168.1.77:8000/api',
+  baseUrl: 'https://capstone-project-iot-1.onrender.com/api',
+  // baseUrl: 'http://192.168.1.77:8000/api',
 };
-  
-  export const IMAGES = {
-    BG_MOBILE:
-      'https://i.pinimg.com/736x/6b/1e/14/6b1e14112f9f7a42b2030f7da534d4b7.jpg',
-    BG_TABLET:
-      'https://i.pinimg.com/736x/44/1f/c8/441fc876392ed7e9a95bf412c04d9f54.jpg',
-    IMAGES_DF:
-      'https://i.pinimg.com/474x/f9/b3/f7/f9b3f7c26702519818f4d31a44922b21.jpg',
-  };
+
+export const IMAGES = {
+  BG_MOBILE:
+    'https://i.pinimg.com/736x/6b/1e/14/6b1e14112f9f7a42b2030f7da534d4b7.jpg',
+  BG_TABLET:
+    'https://i.pinimg.com/736x/44/1f/c8/441fc876392ed7e9a95bf412c04d9f54.jpg',
+  IMAGES_DF:
+    'https://i.pinimg.com/474x/f9/b3/f7/f9b3f7c26702519818f4d31a44922b21.jpg',
+};
